dade51e0734cd48dfa89f588c9b353118e245e95
Shuhei Yoshida <aneki525@gmail.com>
2022-04-29 16:39:33 +0900

    add arc 004 a

---
+++ ARC004/a.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+struct point {
+    int x;
+    int y;
+};
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<point> input_data;
+
+    for (int i = 0; i < N; i++) {
+
+        input_data.push_back()
+    }
+    return 0;
+}
